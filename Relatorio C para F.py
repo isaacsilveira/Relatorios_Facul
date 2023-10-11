13095916470de6f93b5bc700d587c737ad10458e
Isaac Silveira <isaacsilveira7@gmail.com>
2023-10-11 18:12:33 -0300

    Add files via upload

---
+++ Relatorio C para F.py
@@ -0,0 +1,13 @@
+
+import math
+# Relatorio
+
+# Sistema para converter Graus Centigrados para Fahrenheit
+
+A = float(input("digite a temperatura em Graus Centigrados:"))
+
+print(f"{A}")
+
+Resposta = float((1.8*A) + 32)
+
+print(f"{A} Centigrados ={Resposta} em Fahrenheit")
---
+++ Relatorio F para C.py
@@ -0,0 +1,12 @@
+import math
+# Relatorio
+
+# Sistema para converter Graus Centigrados para Fahrenheit
+
+A = float(input("digite a temperatura em Graus Fahrenheit:"))
+
+print(f"{A}")
+
+Resposta = float((A-32)/1.8)
+
+print(f"{A} Fahrenheit ={Resposta} em Centigrados ")
